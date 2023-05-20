9787e0503230f147d22242c928825cbfda2b5d0c
John Quiachon <rjohnquia@gmail.com>
2023-05-20 21:25:23 +0800

    installed styled components extension

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -11,7 +11,7 @@ const Card = (props) => {
   console.log(props)
   return (
    <CardContainer>
-    <img  src={props.image}alt="image" />
+    {/* <img  src={props.image}alt="image" /> */}
     <h1>{props.title}</h1>
     <p>{props.description}</p>
     <Button text="Read More"/>
